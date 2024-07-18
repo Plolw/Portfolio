ea8f650232c7f7b6de8b5cd75f477dc4eba96909
Plolw <polpolortiz@gmail.com>
2024-07-18 18:14:41 +0200

    oh

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,13 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className=''>Ho</div>
+    <div className='text-3xl'>Hola</div>
   )
 }
 
