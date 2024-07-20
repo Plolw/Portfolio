f34ade583a6a1a0202b978df0ad7673434fdbcb9
Plolw <115927381+Plolw@users.noreply.github.com>
2024-07-20 23:06:25 +0200

    huji

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,6 +1,19 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
 const App = () => {
   return (
-    <div className='text-3xl'>Hola</div>
+    <div id="canvas-container" className='App'>
+      <Canvas shadows>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="white" position={[0, 0, 5]} />
+        <mesh rotation={[Math.PI / 8, 10, 10]}>
+          <boxGeometry args={[3, 3, 3]} />
+          <meshStandardMaterial />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
   )
 }
 
