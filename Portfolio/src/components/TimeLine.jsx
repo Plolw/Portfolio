90bb31a9034809c2a13b0b0bfcf75b13246208d8
Plolw <115927381+Plolw@users.noreply.github.com>
2024-09-03 13:03:58 +0200

    qwe

--- Portfolio/src/components/TimeLine.jsx
+++ Portfolio/src/components/TimeLine.jsx
@@ -4,14 +4,24 @@ import TimeCubeCanvas from './TimeCubeCanvas'
 
 const TimeLine = ({ timesData, setCurrentDate }) => {
     return (
-        <div className='flex flex-row justify-center'>
-            {timesData.map((time, index) => (
-                <React.Fragment key={index}>
-                    <TimeCubeCanvas time={time} setCurrentDate={setCurrentDate} />
-                    {index < timesData.length - 1 && <div className='bg-secondary w-32 h-1 self-center'></div>}
-                </React.Fragment>
-            ))}
-        </div>
+        <>
+            <div className='flex flex-row justify-center'>
+                {timesData.map((time, index) => (
+                    <React.Fragment key={index}>
+                        <TimeCubeCanvas time={time} setCurrentDate={setCurrentDate} />
+                        {index < timesData.length - 1 && <div className='bg-secondary w-32 h-1 self-center'></div>}
+                    </React.Fragment>
+                ))}
+            </div>
+            {/* <div className='flex flex-row justify-center'>
+                {timesData.map((time, index) => (
+                    <React.Fragment key={index}>
+                        <p className='w-36'>{time.data.getFullYear()}, {time.data.getMonth()}</p>
+                        {index < timesData.length - 1 && <div className='bg-secondary w-32 self-center'></div>}
+                    </React.Fragment>
+                ))}
+            </div> */}
+        </>
     )
 }
 
--- Portfolio/src/pages/AboutMe.jsx
+++ Portfolio/src/pages/AboutMe.jsx
@@ -3,9 +3,14 @@ import TimeCard from '../components/TimeCard';
 import { useState } from 'react';
 
 const AboutMe = () => {
+    const monthNames = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
     const times = [
         {
-            prefix: "CubeMTLS/cube1", imgUrl: "AboutMeImages/cs50port.png", date: "August 2022", desc: `I wanted to start learning how to code, 
+            prefix: "CubeMTLS/cube1", imgUrl: "AboutMeImages/cs50port.png", date: new Date(2022, 0, 25), desc: `I wanted to start learning how to code, 
             so I enrolled in CS50, a course offered by Harvard university that teaches the basics of computer science through a 
             10 week program. You end up knowing a fair bit of C and data structures, and html, css and Js for Web Development with Flask(Python) and SQLite3 as a backend.` },
         { prefix: "CubeMTLS/cube2", imgUrl: "AboutMeImages/cs50port.png", date: "", desc: "" },
@@ -18,7 +23,7 @@ const AboutMe = () => {
 
     return (
         <div className='flex flex-col h-full 3xl:pt-14 gap-20 w-full'>
-            <TimeCard timeData={currentDate} />
+            <TimeCard timeData={currentDate} monthName={monthNames[currentDate.date.getMonth()]} />
             <TimeLine timesData={times} setCurrentDate={setCurrentDate} />
         </div>
     )
