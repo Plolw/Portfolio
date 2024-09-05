c427e0a71117a224c4988f4f23050cd24606280f
Plolw <115927381+Plolw@users.noreply.github.com>
2024-09-05 21:27:54 +0200

    df

--- Portfolio/src/pages/Projects.jsx
+++ Portfolio/src/pages/Projects.jsx
@@ -7,11 +7,11 @@ import SectionTitle from '../components/SectionTitle';
 const Projects = () => {
 
     const projects = [
-        { url: "ProjectsImages/Grades/Grades2.PNG", name: "GrAdes", desc: "A website to store and manage your grades.", cols: 4 },
-        { url: "ProjectsImages/Kegel/Kegel.png", name: "Kegel", desc: "A 2D platformer videogame made with a team of people (in progress).", cols: 4 },
+        { url: "ProjectsImages/Works/works1.PNG", name: "Works", desc: "Website to create your workouts and store your progress (possibly upgrading to a social network soon).", cols: 4 },
+        { url: "ProjectsImages/Kegel/kegleImg.PNG", name: "Kegle", desc: "A 2D platformer videogame made with a team of people (in progress).", cols: 4 },
         { url: "ProjectsImages/E-Commerce/Ecommerce.PNG", name: "E-Commerce", desc: "Simple e-commerce website to practice secure crud operations.", cols: 2 },
         { url: "ProjectsImages/Social/Social.PNG", name: "Social Network", desc: "A social network where users can publish messages, follow and like each other.", cols: 2 },
-        { url: "ProjectsImages/Works/works1.PNG", name: "Works", desc: "Website to create your workouts and store your progress (possibly upgrading to a social network soon).", cols: 4 },
+        { url: "ProjectsImages/Grades/Grades2.PNG", name: "GrAdes", desc: "A website to store and manage your grades.", cols: 4 },
         { url: "ProjectsImages/Trello/trello.PNG", name: "Trello-like website", desc: "Just like Trello, it allows you to create projects and manage tasks inside of them with your colleagues.", cols: 3 },
         { url: "ProjectsImages/E-mail/Email.PNG", name: "E-mail", desc: "A simple email service.", cols: 2 },
         { url: "ProjectsImages/TRC/TRC.PNG", name: "The random color", desc: "Webapp to gain colors inspiration for your projects.", cols: 3 }
