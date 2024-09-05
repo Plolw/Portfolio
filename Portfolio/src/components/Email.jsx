4b0ca25a8704f3998c3f8f2c3de622fabf328665
Plolw <115927381+Plolw@users.noreply.github.com>
2024-09-05 11:18:01 +0200

    qwe

--- Portfolio/src/components/Email.jsx
+++ Portfolio/src/components/Email.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 const Email = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                process.env.REACT_APP_EMAILJS_SERVICE_ID,
+                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+                form.current,
+                process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+            )
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                }
+            );
+    };
+
     return (
-        <div>Email</div>
+        <form ref={form} onSubmit={sendEmail} className='flex flex-col'>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
     )
 }
 
