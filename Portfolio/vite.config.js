bf1a69ea58cf3a4122edff1034784ac6ab845493
Plolw <115927381+Plolw@users.noreply.github.com>
2024-09-12 21:19:10 +0200

    qwe

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({server: {
-  watch: {
-    usePolling: true,
+export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
   },
-},
   plugins: [react()],
+  base: "/Portfolio/Portfolio/",
 })
